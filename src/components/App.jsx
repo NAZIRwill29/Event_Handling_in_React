0cdc728ee3ba21831d0d7d495f57fd2bf90fa2d1
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 16:15:02 +0800

    event handler like hover in css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //set heading
+  let [heading, setHeading] = useState("Hello!");
+
+  function afterClick() {
+    return setHeading("Submitted");
+  }
+
+  let [btnColor, setBtnColor] = useState("white");
+  // func after mouse hover
+  function afterMouseOver() {
+    return setBtnColor("black");
+  }
+  // func after mouse out
+  function afterMouseOut() {
+    return setBtnColor("white");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={afterClick}
+        onMouseOver={afterMouseOver}
+        onMouseOut={afterMouseOut}
+        style={{ backgroundColor: btnColor }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
